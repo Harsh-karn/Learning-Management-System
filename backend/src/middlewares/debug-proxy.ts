ae988f683c91841b63629ce94d9afeb9329788b6
Harsh <harshkarn12345@gmail.com>
2026-08-27 20:30:17 +0530

    fix: force koa app proxy to true

--- backend/src/middlewares/debug-proxy.ts
+++ backend/src/middlewares/debug-proxy.ts
@@ -1,5 +1,8 @@
 export default () => {
   return async (ctx: any, next: any) => {
+    // Forcefully enable proxy trust for Koa if it wasn't set by Strapi config
+    ctx.app.proxy = true;
+
     if (ctx.request.path === '/api/debug-proxy') {
       ctx.body = {
         secure: ctx.secure,
